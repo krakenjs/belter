7785a4086cf71dbf704f0d84a62805418e19c1ed
Daniel Brain <dbrain@paypal.com>
2018-08-14 17:58:55 -0700

    Add JSON types

--- src/types.js
+++ src/types.js
@@ -1,3 +1,7 @@
 /* @flow */
 
 declare var __TEST__ : boolean;
+
+export type JSONPrimitive = string | boolean | number;
+export type JSONObject = { [string] : JSONPrimitive | JSONObject } | Array<JSONPrimitive | JSONObject>;
+export type JSONType = JSONObject | JSONPrimitive;
