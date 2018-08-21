c90d828f2fb83085857171243a57432ff5790242
Daniel Brain <dbrain@paypal.com>
2018-08-21 14:50:14 -0700

    Fix module name

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@
 
 import { getWebpackConfig } from 'grumbler-scripts/config/webpack.config';
 
-const FILE_NAME = 'mylibrary';
-const MODULE_NAME = 'mylibrary';
+const FILE_NAME = 'belter';
+const MODULE_NAME = 'belter';
 
 export let WEBPACK_CONFIG = getWebpackConfig({
     filename:   `${ FILE_NAME }.js`,
