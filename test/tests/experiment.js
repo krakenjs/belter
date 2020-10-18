85c0b3773874ba92f5ba798fef8cd811986115bc
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-10-18 12:05:27 -0700

    updates #10 - add tests for experiment.js and global.js

--- test/tests/experiment.js
+++ test/tests/experiment.js
@@ -2,110 +2,89 @@
 
 import { experiment } from '../../src/experiment';
 
-// also should check if each are functions too
-const allKeys = (res) => {
-    if (res.hasOwnProperty('isEnabled') && res.hasOwnProperty('isDisabled') && res.hasOwnProperty('getTreatment')) {
-        if (res.hasOwnProperty('log') && res.hasOwnProperty('logStart') && res.hasOwnProperty('logComplete')) {
-            return true;
-        }
-    }
-    return false;
-};
-
 describe('experiment', () => {
-    it('should return an object with all the correct keys when calling log function', () => {
-        const obj = experiment({
-            name:          'chickenLicker'
-        });
-        const res = obj.log('chickenLicker');
-        const allThere = allKeys(res);
-        if (!allThere) {
-            throw new Error(`hinga my dinga ${ JSON.stringify(res) }`);
-        }
-    });
-    it('should return an object with all the correct keys when calling logComplete function', () => {
-        const obj = experiment({
-            name:          'chickenLicker'
-        });
+    const name = 'potatoLicker';
+    it('should return logComplete function that returns the result of experiment function call', () => {
+        const obj = experiment({ name });
         const res = obj.logComplete({});
-        const allThere = allKeys(res);
-        if (!allThere) {
-            throw new Error(`hinga my dinga ${ JSON.stringify(res) }`);
+        if (obj !== res) {
+            throw new Error(`Expected calling logComplete function to equal object returned from experiment function call`);
         }
     });
-    it('should return an object with all the correct keys when calling logStart function', () => {
-        const obj = experiment({
-            name:          'chickenLicker'
-        });
+    it('should return logStart function that returns the result of experiment function call', () => {
+        const obj = experiment({ name });
         const res = obj.logStart({});
-        const allThere = allKeys(res);
-        if (!allThere) {
-            throw new Error(`hinga my dinga ${ JSON.stringify(res) }`);
+        if (obj !== res) {
+            throw new Error(`Expected calling logStart function to equal object returned from experiment function call`);
+        }
+    });
+    it('should call logTreatment function', () => {
+        let isCalled;
+        const logTreatment = () => (isCalled = true);
+        window.localStorage.setItem('__belter_experiment_storage__', JSON.stringify({
+            __session__: {
+                state: {
+                    loggedBeacons: [ 'potatoLicker_potatoLicker_control_undefined' ]
+                }
+            }
+        }));
+        const obj = experiment({ name, logTreatment, sample: 100 });
+        obj.logStart();
+        obj.log('potatoLicker');
+        if (!isCalled) {
+            throw new Error(`Expected logTreatment function to have been called`);
         }
     });
     it('should return true when isDisabled is called', () => {
-        const res = experiment({
-            name:          'chickenLicker'
-        });
+        const res = experiment({ name });
         const { isDisabled } = res;
         const bool = isDisabled();
         if (!bool) {
-            throw new Error(`Expected false, received ${ JSON.stringify(bool) }`);
+            throw new Error(`Expected true, received ${ JSON.stringify(bool) }`);
         }
     });
-    it('should return false correct treatment when isEnabled is called', () => {
-        const res = experiment({
-            name:          'chickenLicker'
-        });
+    it('should return false when isDisabled is called with set localStorage', () => {
+        window.localStorage.potatoLicker = 'hi';
+        const res = experiment({ name });
+        const { isDisabled } = res;
+        const bool = isDisabled();
+        if (bool) {
+            throw new Error(`Expected false, received ${ JSON.stringify(bool) }`);
+        }
+        window.localStorage.removeItem('potatoLicker');
+    });
+    it('should return false when isEnabled is called', () => {
+        const res = experiment({ name });
         const { isEnabled } = res;
         const bool = isEnabled();
         if (bool) {
             throw new Error(`Expected false, received ${ JSON.stringify(bool) }`);
         }
     });
-    it('should return true when isEnabled is called', () => {
-        window.localStorage.chickenLicker = 'hi';
-        const res = experiment({
-            name:          'chickenLicker'
-        });
+    it('should return true when isEnabled is called with set localStorage', () => {
+        window.localStorage.potatoLicker = 'hi';
+        const res = experiment({ name });
         const { isEnabled } = res;
         const bool = isEnabled();
         if (!bool) {
             throw new Error(`Expected true, received ${ JSON.stringify(bool) }`);
         }
+        window.localStorage.removeItem('potatoLicker');
     });
-    it('should return chicken_control when getTreatment is called', () => {
-        const res = experiment({
-            name:          'chickenLicker',
-            sample:        100
-        });
+    it('should return potato_control when getTreatment is called with sample 100', () => {
+        const res = experiment({ name, sample: 100 });
         const { getTreatment } = res;
         const hi = getTreatment();
-        if (hi !== 'chickenLicker_control') {
-            throw new Error(`Expected chickenLicker_control, received ${ hi }`);
+        if (hi !== 'potatoLicker_control') {
+            throw new Error(`Expected potatoLicker_control, received ${ hi }`);
         }
     });
-    it('should return chickenLicker_throttle when getTreatment is called', () => {
-        const res = experiment({
-            name:          'chickenLicker',
-            sample:        0
-        });
+    it('should return potatoLicker_throttle when getTreatment is called with sample 0', () => {
+        const res = experiment({ name, sample: 0 });
         const { getTreatment } = res;
         const hi = getTreatment();
-        if (hi !== 'chickenLicker_throttle') {
-            throw new Error(`Expected chickenLicker_throttle, received ${ hi }`);
+        if (hi !== 'potatoLicker_throttle') {
+            throw new Error(`Expected potatoLicker_throttle, received ${ hi }`);
         }
     });
-    // it('should return chicken_test when getTreatment is called', () => {
-    //     const res = experiment({
-    //         name:          'chickenLicker',
-    //         sample:        50
-    //     });
-    //     const { getTreatment } = res;
-    //     const hi = getTreatment();
-    //     if (hi !== 'chickenLicker_test') {
-    //         throw new Error(`Expected chickenLicker_test, received ${ hi }`);
-    //     }
-    // });
 });
-
--- test/tests/global.js
+++ test/tests/global.js
@@ -4,19 +4,21 @@ import { getGlobalNameSpace } from '../../src/global';
 
 describe('experiment', () => {
     it('should return the right value from the namespace', () => {
-        window.__goku__latest_global__ = { vegeta: 'vegetable' };
+        window.__goku__latest_global__ = { vegeta: 'kamehameha' };
         const { get } = getGlobalNameSpace({ name: 'goku' });
         const res = get('vegeta');
-        if (res !== 'vegetable') {
-            throw new Error(`Expected vegetable, received ${ JSON.stringify(res) }`);
+        if (res !== 'kamehameha') {
+            throw new Error(`Expected kamehameha, received ${ JSON.stringify(res) }`);
         }
+        delete window.__goku__latest_global__;
     });
     it('should return default value from the namespace', () => {
         window.__goku__latest_global__ = null;
         const { get } = getGlobalNameSpace({ name: 'goku' });
         const res = get('vegeta', 'testingDatDefaultValue');
-        if (res !== 'testingDefaultValue') {
-            throw new Error(`Expected testingDefaultValue, received ${ JSON.stringify(res) }`);
+        if (res !== 'testingDatDefaultValue') {
+            throw new Error(`Expected testingDatDefaultValue, received ${ JSON.stringify(res) }`);
         }
+        delete window.__goku__latest_global__;
     });
 });
