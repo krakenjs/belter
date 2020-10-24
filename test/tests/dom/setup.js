4347429699a2ba631034f48877d51fd20c18c7f5
cijin <cijininurhood@gmail.com>
2020-10-24 11:32:07 +0700

    tests updated for waitForDocumentReady

--- test/tests/dom/setup.js
+++ test/tests/dom/setup.js
@@ -3,12 +3,20 @@
     - this allows us to switch readyState between 'loading', 'complete' and 'interactive'
     - the reason for adding this here is for consistency
     - else only the tests defined after the property below has been defined will be able to set values for readyState
+
+    When to use document.mockReadyState?
+    - When testing cases when the browser is ready or interactive after a setInterval
+    - Even if you set document.readyState to 'loading', the browser will eventually load
+    - Although your tests would pass but not for the reasons you were testing them for
 */
 
 let oldReadyState = document.readyState;
 
 Object.defineProperty(document, 'readyState', {
     get() : string {
+        if (document.mockReadyState) {
+            return document.mockReadyState;
+        }
         return oldReadyState;
     },
 
--- test/tests/dom/waitForDocumentReady.js
+++ test/tests/dom/waitForDocumentReady.js
@@ -3,58 +3,29 @@
 import { waitForDocumentReady } from '../../../src/dom';
 
 describe('waitForDocumentReady cases', () => {
-    const oldState = document.readyState;
-
-    afterEach(() => {
-        document.readyState = oldState;
-    });
-
-    it('should resolve when document is ready', async () => {
-        try {
-            document.readyState = 'complete';
-            await waitForDocumentReady();
-        } catch (err) {
-            throw new Error(
-                'Expected waitForDocumentReady to resolve when document is ready'
-            );
-        }
-    });
-
-    it('should eventully resolve when document is ready', async () => {
-        try {
-            document.readyState = 'loading';
-            setTimeout(() => {
-                document.readyState = 'complete';
-            }, 100);
-            await waitForDocumentReady();
-        } catch (err) {
-            throw new Error(
-                'Expected waitForDocumentReady to eventully resolve when document is ready'
-            );
-        }
-    });
-
     it('should resolve when document is interactive', async () => {
         try {
             document.readyState = 'interactive';
             await waitForDocumentReady();
         } catch (err) {
-            throw new Error(
-                'Expected waitForDocumentReady to resolve when document is interactive'
-            );
+            throw new Error('Expected waitForDocumentReady to resolve');
         }
     });
 
-    it('should eventully resolve when document is interactive', async () => {
+    it('should eventully resolve when document is ready', async () => {
         try {
-            document.readyState = 'loading';
+            document.mockReadyState = 'loading';
+
             setTimeout(() => {
-                document.readyState = 'interactive';
-            }, 100);
-            await waitForDocumentReady();
+                document.mockReadyState = false;
+                document.readyState = 'complete';
+            }, 20);
+
+            // the argument 'test' is passed in just to bust the cache, as the function is memoized
+            await waitForDocumentReady('test');
         } catch (err) {
             throw new Error(
-                'Expected waitForDocumentReady to eventully resolve when document is interactive'
+                `Expected waitForDocumentReady to eventully resolve when document is ready`
             );
         }
     });
