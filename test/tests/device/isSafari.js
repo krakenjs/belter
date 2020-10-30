55157a20c63f5ef9cb060047d096ffbe1a530c55
khoa <khoa@Anthonys-MacBook-Pro.local>
2020-10-29 20:53:52 -0700

    updates #10 - add test files

---
+++ test/tests/device/isSafari.js
@@ -0,0 +1,30 @@
+/* @flow */
+
+import { isSafari  } from '../../../src/device';
+
+describe('isSafari', () => {
+    it('should return true when userAgent contains Safari and isChrome function returns false', () => {
+        // eslint-disable-next-line compat/compat
+        window.navigator.userAgent = 'Safari';
+        const bool = isSafari();
+        if (!bool) {
+            throw new Error(`Expected true, got ${ JSON.stringify(bool) }`);
+        }
+    });
+    it('should return false when userAgent contains Safari and isChrome function returns false', () => {
+        // eslint-disable-next-line compat/compat
+        window.navigator.userAgent = 'SafariChrome';
+        const bool = isSafari();
+        if (bool) {
+            throw new Error(`Expected false, got ${ JSON.stringify(bool) }`);
+        }
+    });
+    it('should return false when userAgent does not contain Safari', () => {
+        // eslint-disable-next-line compat/compat
+        window.navigator.userAgent = 'potato';
+        const bool = isSafari();
+        if (bool) {
+            throw new Error(`Expected false, got ${ JSON.stringify(bool) }`);
+        }
+    });
+});
