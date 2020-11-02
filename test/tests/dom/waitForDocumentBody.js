4e77fdd5c28fee67c338ad132dba152ee64ed0bd
cijin <cijininurhood@gmail.com>
2020-11-02 11:12:00 +0700

    fixed lint errors

--- test/tests/dom/waitForDocumentBody.js
+++ test/tests/dom/waitForDocumentBody.js
@@ -13,36 +13,28 @@ describe('waitForDocumentBody cases', () => {
         document.body = oldBody;
     });
     it('should resolve when body is present', async () => {
-        try {
-            document.readyState = 'complete';
-            document.body = testBody;
-            const result = await waitForDocumentBody();
+        document.readyState = 'complete';
+        document.body = testBody;
+        const result = await waitForDocumentBody();
 
-            if (result !== testBody) {
-                throw new Error('Expected result to be the same as testBody');
-            }
-        } catch (err) {
-            throw new Error(` ${ err }`);
+        if (result !== testBody) {
+            throw new Error('Expected result to be the same as testBody');
         }
     });
 
     it('should eventully resolve when document is ready', async () => {
-        try {
-            document.readyState = 'loading';
-            document.body = null;
+        document.readyState = 'loading';
+        document.body = null;
 
-            setTimeout(() => {
-                document.readyState = 'complete';
-                document.body = testBody;
-            }, 20);
+        setTimeout(() => {
+            document.readyState = 'complete';
+            document.body = testBody;
+        }, 20);
 
-            const result = await waitForDocumentBody();
+        const result = await waitForDocumentBody();
 
-            if (result !== testBody) {
-                throw new Error('Expected result to be the same as testBody');
-            }
-        } catch (err) {
-            throw new Error(` ${ err }`);
+        if (result !== testBody) {
+            throw new Error('Expected result to be the same as testBody');
         }
     });
 });
