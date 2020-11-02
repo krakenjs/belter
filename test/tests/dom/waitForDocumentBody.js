315c350ec722e64b47e87d77434df014e012dffc
Cijin Cherian <cijininurhood@gmail.com>
2020-11-02 09:11:09 +0700

    Removed try, catch

--- test/tests/dom/waitForDocumentBody.js
+++ test/tests/dom/waitForDocumentBody.js
@@ -12,37 +12,30 @@ describe('waitForDocumentBody cases', () => {
     afterEach(() => {
         document.body = oldBody;
     });
-    it('should resolve when body is present', async () => {
-        try {
-            document.readyState = 'complete';
-            document.body = testBody;
-            const result = await waitForDocumentBody();
+    
+    it('should resolve when body is present', async () => {       
+        document.readyState = 'complete';
+        document.body = testBody;
+        const result = await waitForDocumentBody();
 
-            if (result !== testBody) {
-                throw new Error('Expected result to be the same as testBody');
-            }
-        } catch (err) {
-            throw new Error(` ${ err }`);
+        if (result !== testBody) {
+            throw new Error('Expected result to be the same as testBody');
         }
     });
 
     it('should eventully resolve when document is ready', async () => {
-        try {
-            document.readyState = 'loading';
-            document.body = null;
+        document.readyState = 'loading';
+        document.body = null;
 
-            setTimeout(() => {
-                document.readyState = 'complete';
-                document.body = testBody;
-            }, 20);
+        setTimeout(() => {
+            document.readyState = 'complete';
+            document.body = testBody;
+        }, 20);
 
-            const result = await waitForDocumentBody();
+        const result = await waitForDocumentBody();
 
-            if (result !== testBody) {
-                throw new Error('Expected result to be the same as testBody');
-            }
-        } catch (err) {
-            throw new Error(` ${ err }`);
-        }
+        if (result !== testBody) {
+            throw new Error('Expected result to be the same as testBody');
+        }            
     });
 });
