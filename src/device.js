b7a2ad3f7f8b4b3b4c1517cdd5eebef3c051312b
Steve Mask <smask@paypal.com>
2021-03-05 14:24:45 -0500

    update regex

--- src/device.js
+++ src/device.js
@@ -15,7 +15,7 @@ export function isDevice(userAgent? : string = getUserAgent()) : boolean {
 
 export function isWebView() : boolean {
     const userAgent = getUserAgent();
-    return (/(iPhone|iPod|iPad|Macintosh).*AppleWebKit(?!.*Safari)/i).test(userAgent) ||
+    return (/(iPhone|iPod|iPad|Macintosh).*AppleWebKit(?!.*Safari)|.*WKWebView/i).test(userAgent) ||
         (/\bwv\b/).test(userAgent) ||
     (/Android.*Version\/(\d)\.(\d)/i).test(userAgent);
 }
