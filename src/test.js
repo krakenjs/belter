4a5f18ca66731affc1f666cfada3d1e1f5becdd0
Daniel Brain <dbrain@paypal.com>
2021-05-17 14:07:31 -0700

    Instantly reject on wrapPromise timeout

--- src/test.js
+++ src/test.js
@@ -13,81 +13,83 @@ export function wrapPromise<T>(method : Wrapper<T>, { timeout = 5000 } : {| time
     const expected : Array<string> = [];
     const promises : Array<ZalgoPromise<*>> = [];
 
-    const timer = setTimeout(() => {
-        if (expected.length) {
-            promises.push(ZalgoPromise.asyncReject(new Error(`Expected ${ expected[0] } to be called`)));
-        }
-    }, timeout);
-
-    const expect : Handler = (name, fn = noop) => {
-        expected.push(name);
-        
-        // $FlowFixMe
-        return function expectWrapper(...args) : * {
-            removeFromArray(expected, name);
-
-            // $FlowFixMe
-            const { result, error } = tryCatch(() => fn.call(this, ...args));
-
-            if (error) {
-                promises.push(ZalgoPromise.asyncReject(error));
-                throw error;
-            }
-
-            promises.push(ZalgoPromise.resolve(result));
-            return result;
-        };
-    };
-
-    const avoid : Handler = (name : string, fn = noop) => {
-
-        // $FlowFixMe
-        return function avoidWrapper(...args) : * {
-            promises.push(ZalgoPromise.asyncReject(new Error(`Expected ${ name } to not be called`)));
-            // $FlowFixMe
-            return fn.call(this, ...args);
-        };
-    };
-
-    const expectError : Handler = (name, fn = noop) => {
-        expected.push(name);
-
-        // $FlowFixMe
-        return function expectErrorWrapper(...args) : * {
-            removeFromArray(expected, name);
-
-            // $FlowFixMe
-            const { result, error } = tryCatch(() => fn.call(this, ...args));
-
-            if (error) {
-                throw error;
-            }
-
-            promises.push(ZalgoPromise.resolve(result).then(() => {
-                throw new Error(`Expected ${ name } to throw an error`);
-            }, noop));
-            return result;
-        };
-    };
-
-    const wait = () => {
-        return ZalgoPromise.try(() => {
-            if (promises.length) {
-                return promises.pop();
-            }
-        }).then(() => {
-            if (promises.length) {
-                return wait();
-            }
+    return new ZalgoPromise((resolve, reject) => {
+        const timer = setTimeout(() => {
             if (expected.length) {
-                return ZalgoPromise.delay(10).then(wait);
+                reject(new Error(`Expected ${ expected[0] } to be called`));
             }
-        });
-    };
+        }, timeout);
 
-    promises.push(ZalgoPromise.try(() => method({ expect, avoid, expectError, error: avoid, wait })));
+        const expect : Handler = (name, fn = noop) => {
+            expected.push(name);
+            
+            // $FlowFixMe
+            return function expectWrapper(...args) : * {
+                removeFromArray(expected, name);
 
-    return wait().then(() => {
-        clearTimeout(timer);
+                // $FlowFixMe
+                const { result, error } = tryCatch(() => fn.call(this, ...args));
+
+                if (error) {
+                    promises.push(ZalgoPromise.asyncReject(error));
+                    throw error;
+                }
+
+                promises.push(ZalgoPromise.resolve(result));
+                return result;
+            };
+        };
+
+        const avoid : Handler = (name : string, fn = noop) => {
+
+            // $FlowFixMe
+            return function avoidWrapper(...args) : * {
+                promises.push(ZalgoPromise.asyncReject(new Error(`Expected ${ name } to not be called`)));
+                // $FlowFixMe
+                return fn.call(this, ...args);
+            };
+        };
+
+        const expectError : Handler = (name, fn = noop) => {
+            expected.push(name);
+
+            // $FlowFixMe
+            return function expectErrorWrapper(...args) : * {
+                removeFromArray(expected, name);
+
+                // $FlowFixMe
+                const { result, error } = tryCatch(() => fn.call(this, ...args));
+
+                if (error) {
+                    throw error;
+                }
+
+                promises.push(ZalgoPromise.resolve(result).then(() => {
+                    throw new Error(`Expected ${ name } to throw an error`);
+                }, noop));
+                return result;
+            };
+        };
+
+        const wait = () => {
+            return ZalgoPromise.try(() => {
+                if (promises.length) {
+                    return promises.pop();
+                }
+            }).then(() => {
+                if (promises.length) {
+                    return wait();
+                }
+                if (expected.length) {
+                    return ZalgoPromise.delay(10).then(wait);
+                }
+            });
+        };
+
+        promises.push(ZalgoPromise.try(() => method({ expect, avoid, expectError, error: avoid, wait })));
+
+        wait().finally(() => {
+            clearTimeout(timer);
+        }).then(resolve, reject);
     });
 }
