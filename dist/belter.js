f5e50d7326233a07aef8192ca815b0727ff02bec
Daniel Brain <dbrain@paypal.com>
2021-05-17 14:12:33 -0700

    Dist

--- dist/belter.js
+++ dist/belter.js
@@ -3117,76 +3117,78 @@
             throw new Error("Can not normalize dimension: " + dim);
         }
         function wrapPromise(method, _temp) {
-            var _ref$timeout = (void 0 === _temp ? {} : _temp).timeout;
+            var _ref$timeout = (void 0 === _temp ? {} : _temp).timeout, timeout = void 0 === _ref$timeout ? 5e3 : _ref$timeout;
             var expected = [];
             var promises = [];
-            var timer = setTimeout((function() {
-                expected.length && promises.push(promise_ZalgoPromise.asyncReject(new Error("Expected " + expected[0] + " to be called")));
-            }), void 0 === _ref$timeout ? 5e3 : _ref$timeout);
-            var expect = function(name, fn) {
-                void 0 === fn && (fn = src_util_noop);
-                expected.push(name);
-                return function() {
-                    var _this = this;
-                    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) args[_key] = arguments[_key];
-                    removeFromArray(expected, name);
-                    var _tryCatch = tryCatch((function() {
-                        var _fn;
-                        return (_fn = fn).call.apply(_fn, [ _this ].concat(args));
-                    })), result = _tryCatch.result, error = _tryCatch.error;
-                    if (error) {
-                        promises.push(promise_ZalgoPromise.asyncReject(error));
-                        throw error;
-                    }
-                    promises.push(promise_ZalgoPromise.resolve(result));
-                    return result;
+            return new promise_ZalgoPromise((function(resolve, reject) {
+                var timer = setTimeout((function() {
+                    expected.length && reject(new Error("Expected " + expected[0] + " to be called"));
+                }), timeout);
+                var expect = function(name, fn) {
+                    void 0 === fn && (fn = src_util_noop);
+                    expected.push(name);
+                    return function() {
+                        var _this = this;
+                        for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) args[_key] = arguments[_key];
+                        removeFromArray(expected, name);
+                        var _tryCatch = tryCatch((function() {
+                            var _fn;
+                            return (_fn = fn).call.apply(_fn, [ _this ].concat(args));
+                        })), result = _tryCatch.result, error = _tryCatch.error;
+                        if (error) {
+                            promises.push(promise_ZalgoPromise.asyncReject(error));
+                            throw error;
+                        }
+                        promises.push(promise_ZalgoPromise.resolve(result));
+                        return result;
+                    };
                 };
-            };
-            var avoid = function(name, fn) {
-                void 0 === fn && (fn = src_util_noop);
-                return function() {
-                    var _fn2;
-                    promises.push(promise_ZalgoPromise.asyncReject(new Error("Expected " + name + " to not be called")));
-                    for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) args[_key2] = arguments[_key2];
-                    return (_fn2 = fn).call.apply(_fn2, [ this ].concat(args));
+                var avoid = function(name, fn) {
+                    void 0 === fn && (fn = src_util_noop);
+                    return function() {
+                        var _fn2;
+                        promises.push(promise_ZalgoPromise.asyncReject(new Error("Expected " + name + " to not be called")));
+                        for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) args[_key2] = arguments[_key2];
+                        return (_fn2 = fn).call.apply(_fn2, [ this ].concat(args));
+                    };
                 };
-            };
-            var expectError = function(name, fn) {
-                void 0 === fn && (fn = src_util_noop);
-                expected.push(name);
-                return function() {
-                    var _this2 = this;
-                    for (var _len3 = arguments.length, args = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) args[_key3] = arguments[_key3];
-                    removeFromArray(expected, name);
-                    var _tryCatch2 = tryCatch((function() {
-                        var _fn3;
-                        return (_fn3 = fn).call.apply(_fn3, [ _this2 ].concat(args));
-                    })), result = _tryCatch2.result, error = _tryCatch2.error;
-                    if (error) throw error;
-                    promises.push(promise_ZalgoPromise.resolve(result).then((function() {
-                        throw new Error("Expected " + name + " to throw an error");
-                    }), src_util_noop));
-                    return result;
+                var expectError = function(name, fn) {
+                    void 0 === fn && (fn = src_util_noop);
+                    expected.push(name);
+                    return function() {
+                        var _this2 = this;
+                        for (var _len3 = arguments.length, args = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) args[_key3] = arguments[_key3];
+                        removeFromArray(expected, name);
+                        var _tryCatch2 = tryCatch((function() {
+                            var _fn3;
+                            return (_fn3 = fn).call.apply(_fn3, [ _this2 ].concat(args));
+                        })), result = _tryCatch2.result, error = _tryCatch2.error;
+                        if (error) throw error;
+                        promises.push(promise_ZalgoPromise.resolve(result).then((function() {
+                            throw new Error("Expected " + name + " to throw an error");
+                        }), src_util_noop));
+                        return result;
+                    };
                 };
-            };
-            var wait = function wait() {
-                return promise_ZalgoPromise.try((function() {
-                    if (promises.length) return promises.pop();
-                })).then((function() {
-                    return promises.length ? wait() : expected.length ? promise_ZalgoPromise.delay(10).then(wait) : void 0;
-                }));
-            };
-            promises.push(promise_ZalgoPromise.try((function() {
-                return method({
-                    expect: expect,
-                    avoid: avoid,
-                    expectError: expectError,
-                    error: avoid,
-                    wait: wait
-                });
-            })));
-            return wait().then((function() {
-                clearTimeout(timer);
+                var wait = function wait() {
+                    return promise_ZalgoPromise.try((function() {
+                        if (promises.length) return promises.pop();
+                    })).then((function() {
+                        return promises.length ? wait() : expected.length ? promise_ZalgoPromise.delay(10).then(wait) : void 0;
+                    }));
+                };
+                promises.push(promise_ZalgoPromise.try((function() {
+                    return method({
+                        expect: expect,
+                        avoid: avoid,
+                        expectError: expectError,
+                        error: avoid,
+                        wait: wait
+                    });
+                })));
+                wait().finally((function() {
+                    clearTimeout(timer);
+                })).then(resolve, reject);
             }));
         }
     } ]);
--- dist/module/test.js
+++ dist/module/test.js
@@ -7,126 +7,128 @@ export function wrapPromise(method, _temp) {
 
   var expected = [];
   var promises = [];
-  var timer = setTimeout(function () {
-    if (expected.length) {
-      promises.push(ZalgoPromise.asyncReject(new Error("Expected " + expected[0] + " to be called")));
-    }
-  }, timeout);
-
-  var expect = function expect(name, fn) {
-    if (fn === void 0) {
-      fn = noop;
-    }
-
-    expected.push(name); // $FlowFixMe
-
-    return function expectWrapper() {
-      var _this = this;
-
-      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-        args[_key] = arguments[_key];
-      }
-
-      removeFromArray(expected, name); // $FlowFixMe
-
-      var _tryCatch = tryCatch(function () {
-        var _fn;
-
-        return (_fn = fn).call.apply(_fn, [_this].concat(args));
-      }),
-          result = _tryCatch.result,
-          error = _tryCatch.error;
-
-      if (error) {
-        promises.push(ZalgoPromise.asyncReject(error));
-        throw error;
-      }
-
-      promises.push(ZalgoPromise.resolve(result));
-      return result;
-    };
-  };
-
-  var avoid = function avoid(name, fn) {
-    if (fn === void 0) {
-      fn = noop;
-    }
-
-    // $FlowFixMe
-    return function avoidWrapper() {
-      var _fn2;
-
-      promises.push(ZalgoPromise.asyncReject(new Error("Expected " + name + " to not be called"))); // $FlowFixMe
-
-      for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
-        args[_key2] = arguments[_key2];
-      }
-
-      return (_fn2 = fn).call.apply(_fn2, [this].concat(args));
-    };
-  };
-
-  var expectError = function expectError(name, fn) {
-    if (fn === void 0) {
-      fn = noop;
-    }
-
-    expected.push(name); // $FlowFixMe
-
-    return function expectErrorWrapper() {
-      var _this2 = this;
-
-      for (var _len3 = arguments.length, args = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
-        args[_key3] = arguments[_key3];
-      }
-
-      removeFromArray(expected, name); // $FlowFixMe
-
-      var _tryCatch2 = tryCatch(function () {
-        var _fn3;
-
-        return (_fn3 = fn).call.apply(_fn3, [_this2].concat(args));
-      }),
-          result = _tryCatch2.result,
-          error = _tryCatch2.error;
-
-      if (error) {
-        throw error;
-      }
-
-      promises.push(ZalgoPromise.resolve(result).then(function () {
-        throw new Error("Expected " + name + " to throw an error");
-      }, noop));
-      return result;
-    };
-  };
-
-  var wait = function wait() {
-    return ZalgoPromise.try(function () {
-      if (promises.length) {
-        return promises.pop();
-      }
-    }).then(function () {
-      if (promises.length) {
-        return wait();
-      }
-
+  return new ZalgoPromise(function (resolve, reject) {
+    var timer = setTimeout(function () {
       if (expected.length) {
-        return ZalgoPromise.delay(10).then(wait);
+        reject(new Error("Expected " + expected[0] + " to be called"));
       }
-    });
-  };
+    }, timeout);
 
-  promises.push(ZalgoPromise.try(function () {
-    return method({
-      expect: expect,
-      avoid: avoid,
-      expectError: expectError,
-      error: avoid,
-      wait: wait
-    });
-  }));
-  return wait().then(function () {
-    clearTimeout(timer);
+    var expect = function expect(name, fn) {
+      if (fn === void 0) {
+        fn = noop;
+      }
+
+      expected.push(name); // $FlowFixMe
+
+      return function expectWrapper() {
+        var _this = this;
+
+        for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+          args[_key] = arguments[_key];
+        }
+
+        removeFromArray(expected, name); // $FlowFixMe
+
+        var _tryCatch = tryCatch(function () {
+          var _fn;
+
+          return (_fn = fn).call.apply(_fn, [_this].concat(args));
+        }),
+            result = _tryCatch.result,
+            error = _tryCatch.error;
+
+        if (error) {
+          promises.push(ZalgoPromise.asyncReject(error));
+          throw error;
+        }
+
+        promises.push(ZalgoPromise.resolve(result));
+        return result;
+      };
+    };
+
+    var avoid = function avoid(name, fn) {
+      if (fn === void 0) {
+        fn = noop;
+      }
+
+      // $FlowFixMe
+      return function avoidWrapper() {
+        var _fn2;
+
+        promises.push(ZalgoPromise.asyncReject(new Error("Expected " + name + " to not be called"))); // $FlowFixMe
+
+        for (var _len2 = arguments.length, args = new Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+          args[_key2] = arguments[_key2];
+        }
+
+        return (_fn2 = fn).call.apply(_fn2, [this].concat(args));
+      };
+    };
+
+    var expectError = function expectError(name, fn) {
+      if (fn === void 0) {
+        fn = noop;
+      }
+
+      expected.push(name); // $FlowFixMe
+
+      return function expectErrorWrapper() {
+        var _this2 = this;
+
+        for (var _len3 = arguments.length, args = new Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+          args[_key3] = arguments[_key3];
+        }
+
+        removeFromArray(expected, name); // $FlowFixMe
+
+        var _tryCatch2 = tryCatch(function () {
+          var _fn3;
+
+          return (_fn3 = fn).call.apply(_fn3, [_this2].concat(args));
+        }),
+            result = _tryCatch2.result,
+            error = _tryCatch2.error;
+
+        if (error) {
+          throw error;
+        }
+
+        promises.push(ZalgoPromise.resolve(result).then(function () {
+          throw new Error("Expected " + name + " to throw an error");
+        }, noop));
+        return result;
+      };
+    };
+
+    var wait = function wait() {
+      return ZalgoPromise.try(function () {
+        if (promises.length) {
+          return promises.pop();
+        }
+      }).then(function () {
+        if (promises.length) {
+          return wait();
+        }
+
+        if (expected.length) {
+          return ZalgoPromise.delay(10).then(wait);
+        }
+      });
+    };
+
+    promises.push(ZalgoPromise.try(function () {
+      return method({
+        expect: expect,
+        avoid: avoid,
+        expectError: expectError,
+        error: avoid,
+        wait: wait
+      });
+    }));
+    wait().finally(function () {
+      clearTimeout(timer);
+    }).then(resolve, reject);
   });
 }
