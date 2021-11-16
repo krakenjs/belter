be03b50cc052fffc50bc827c63564bf29cc0b3c7
Steve Mask <smask@paypal.com>
2021-11-16 14:01:27 -0500

    remove test from ios

--- test/tests/device/isIos.js
+++ test/tests/device/isIos.js
@@ -39,12 +39,4 @@ describe('isIos', () => {
             throw new Error(`Expected false, got ${ JSON.stringify(bool) }`);
         }
     });
-    it('should return false when userAgent is Yandex browser', () => {
-        
-        window.navigator.userAgent = 'Mozilla/5.0 (iPad; CPU OS 14_8 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 YaBrowser/21.11.1.882.11 Mobile/15E148 Safari/604.1';
-        const bool = isIos();
-        if (bool) {
-            throw new Error(`Expected false, got ${ JSON.stringify(bool) }`);
-        }
-    });
 });
