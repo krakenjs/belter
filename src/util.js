f5437563dfaab58cc365aeb434e3f7d7fcd60a6f
prashantjagasia <108553844+prashantjagasia@users.noreply.github.com>
2023-08-22 11:53:49 -0700

    added url-sanitization function

--- src/util.js
+++ src/util.js
@@ -1370,3 +1370,40 @@ export class ExtendableError extends Error {
     }
   }
 }
+
+export function sanitizeUrl(url?: string): string {
+  const BLANK_URL = "about:blank";
+  const htmlCtrlEntityRegex = /&(newline|tab);/gi;
+  const ctrlCharactersRegex = /[\u0000-\u001F\u007F-\u009F\u2000-\u200D\uFEFF]/gim;
+  
+  if (!url) {
+    return BLANK_URL;
+  }
+
+  const sanitizedUrl = decodeHtmlCharacters(url)
+    .replace(htmlCtrlEntityRegex, "")
+    .replace(ctrlCharactersRegex, "")
+    .trim();
+
+  if (!sanitizedUrl) {
+    return BLANK_URL;
+  }
+
+  if (isRelativeUrlWithoutProtocol(sanitizedUrl)) {
+    return sanitizedUrl;
+  }
+
+  const urlSchemeParseResults = sanitizedUrl.match(urlSchemeRegex);
+
+  if (!urlSchemeParseResults) {
+    return sanitizedUrl;
+  }
+
+  const urlScheme = urlSchemeParseResults[0];
+
+  if (invalidProtocolRegex.test(urlScheme)) {
+    return BLANK_URL;
+  }
+
+  return sanitizedUrl;
+}
