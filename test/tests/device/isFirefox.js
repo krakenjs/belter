48f5dc1aacc99edbabea4359fafdb93e6435fc0a
Ravi Shekhar <ravishekhar@users.noreply.github.com>
2025-02-13 10:17:57 -0600

    fix: disable popups for firefox on android (#120)
    * fix: disable popups for firefox on android
    
    * fix: ignore hermes parser from type check
    
    * ci: check in dist folder
    
    * chore(release): 2.7.0
    
    * Revert "chore(release): 2.7.0"
    
    This reverts commit e31976a28332b023662fb334b15fc9e2be033b95.
    
    * Revert "ci: check in dist folder"
    
    This reverts commit aec5c0104995762ed56921e0e8b06a3ecceca10f.
    
    * fix: update version to avoid conflicts with existing 2.7.0

--- test/tests/device/isFirefox.js
+++ test/tests/device/isFirefox.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { isFirefox } from "../../../src/device";
+import { isFirefox, isFirefoxAndroid } from "../../../src/device";
 
 describe("isFirefoxIOS", () => {
   beforeEach(() => {
@@ -17,6 +17,27 @@ describe("isFirefoxIOS", () => {
   it("should return false when userAgent does NOT contain firefox(case insensitive)", () => {
     window.navigator.userAgent = "fired potato";
     const bool = isFirefox();
+    if (bool) {
+      throw new Error(`Expected false, got ${JSON.stringify(bool)}`);
+    }
+  });
+});
+
+describe("isFirefoxAndroid", () => {
+  beforeEach(() => {
+    window.navigator = {};
+  });
+  it("should return true when userAgent contains firefox(case insensitive)", () => {
+    window.navigator.userAgent =
+      "Mozilla/5.0 (Android 15; Mobile; rv:135.0) Gecko/135.0 Firefox/135.0";
+    const bool = isFirefoxAndroid();
+    if (!bool) {
+      throw new Error(`Expected true, got ${JSON.stringify(bool)}`);
+    }
+  });
+  it("should return false when userAgent does NOT contain firefox(case insensitive)", () => {
+    window.navigator.userAgent = "fired potato";
+    const bool = isFirefoxAndroid();
     if (bool) {
       throw new Error(`Expected false, got ${JSON.stringify(bool)}`);
     }
